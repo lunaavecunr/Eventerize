bfde617f903d56dfc7a904b7447263f725b8dd76
theo <theo.tanchoux@etu.univ-lyon1.fr>
2019-05-02 13:52:14 +0200

    [FEAT] Log in data

--- app/src/main/java/com/luna/eventerize/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/luna/eventerize/presentation/ui/activity/MainActivity.kt
@@ -2,12 +2,15 @@ package com.luna.eventerize.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luna.eventerize.EventerizeApp
 import com.luna.eventerize.R
 
 class MainActivity : AppCompatActivity() {
+    private var repository = EventerizeApp.getInstance().repository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        repository.login("dfghjkl", "test123")
     }
 }
