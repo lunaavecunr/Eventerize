57932fa75ecc648dfb60eeb428137883985c486e
Jordan Bonnot <jojobonnot@gmail.com>
2019-05-02 14:00:27 +0200

    fix top bar color

--- app/src/main/java/com/luna/eventerize/presentation/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/luna/eventerize/presentation/ui/fragments/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.luna.eventerize.R
 import com.luna.eventerize.presentation.ui.fragments.base.BaseFragment
 import com.luna.eventerize.presentation.viewmodel.LoginViewModel
@@ -14,19 +15,23 @@ class LoginFragment : BaseFragment<LoginViewModel>() {
     override val viewModelClass = LoginViewModel::class
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         fragment_login_loginButton.setOnClickListener {
             verifyFieldsAndLogin()
         }
 
 
-        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     fun verifyFieldsAndLogin() {
         if(fragment_login_emailField.text!!.isEmpty() || fragment_login_passwordField.text!!.isEmpty()){
-
+            Toast.makeText(context, getString(R.string.fillAllFields), Toast.LENGTH_SHORT).show()
         } else {
-            
+
         }
     }
 
