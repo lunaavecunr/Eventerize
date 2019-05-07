610dbf9214a011e291e8f6c6a64e9deb99656d78
Jordan Bonnot <jojobonnot@gmail.com>
2019-05-07 16:27:17 +0200

    [FEAT] Add Share options
    add share options, waiting for details screen and QRCode implementation

--- app/src/main/java/com/luna/eventerize/presentation/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/luna/eventerize/presentation/ui/fragments/LoginFragment.kt
@@ -1,6 +1,10 @@
 package com.luna.eventerize.presentation.ui.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,12 +17,18 @@ import com.luna.eventerize.presentation.utils.showError
 import com.luna.eventerize.presentation.viewmodel.LoginViewModel
 import com.parse.ParseUser
 import kotlinx.android.synthetic.main.fragment_login.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class LoginFragment : BaseFragment<LoginViewModel>(), View.OnClickListener {
 
     override val viewModelClass = LoginViewModel::class
     lateinit var navigator: Navigator
+    private val SHARE_QR_CODE = 1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
@@ -30,6 +40,7 @@ class LoginFragment : BaseFragment<LoginViewModel>(), View.OnClickListener {
         navigator = Navigator(fragmentManager!!)
         fragment_login_loginButton.setOnClickListener(this)
         fragment_login_createAccountTV.setOnClickListener(this)
+        share_Button.setOnClickListener(this)
 
         val updateError = Observer<EventerizeError>{
             showError(activity!!, it.message)
@@ -52,10 +63,42 @@ class LoginFragment : BaseFragment<LoginViewModel>(), View.OnClickListener {
            R.id.fragment_login_createAccountTV -> {
                navigator.displaySignUp()
            }
+           R.id.share_Button -> {
+               val sharingIntent = Intent(android.content.Intent.ACTION_SEND)
+               sharingIntent.type = "*/*"
+               val shareBodyText = "https://google.com"
+               sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Viens à mon event")
+               sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBodyText)
+               //val qrCodeBitmap : Bitmap = null
+               val bytes = ByteArrayOutputStream()
+               //qrCodeBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+               val f = File(Environment.getExternalStorageDirectory(),separator + "qrcode.jpg")
+               try {
+                   f.createNewFile()
+                   val fo = FileOutputStream(f)
+                   fo.write(bytes.toByteArray())
+               } catch (e: IOException) {
+                   e.printStackTrace()
+               }
+
+               sharingIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:///sdcard/qrcode.jpg"))
+               startActivityForResult(Intent.createChooser(sharingIntent, "Sharing Options"), SHARE_QR_CODE)
+
+           }
 
        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == SHARE_QR_CODE) {
+            if(resultCode == Activity.RESULT_OK){
+                val f = File(Environment.getExternalStorageDirectory(),separator + "qrcode.jpg")
+                f.delete()
+            }
+        }
+
+    }
 
 
 }
