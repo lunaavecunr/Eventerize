c50a3ab9726f52ea0b44854f71345028fb5fb474
Brandon <bigoud33@gmail.com>
2019-05-09 15:44:22 +0200

    [FIX] Fix floating button

--- app/src/main/java/com/luna/eventerize/presentation/ui/fragments/TabsListFragment.kt
+++ app/src/main/java/com/luna/eventerize/presentation/ui/fragments/TabsListFragment.kt
@@ -1,11 +1,20 @@
 package com.luna.eventerize.presentation.ui.fragments
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
+import com.livinglifetechway.quickpermissions_kotlin.util.QuickPermissionsOptions
+import com.livinglifetechway.quickpermissions_kotlin.util.QuickPermissionsRequest
 import com.luna.eventerize.R
 import com.luna.eventerize.presentation.navigator.Navigator
 import com.luna.eventerize.presentation.ui.adapter.TabsListAdapter
@@ -14,7 +23,7 @@ import com.luna.eventerize.presentation.viewmodel.TabsListViewModel
 import kotlinx.android.synthetic.main.fragment_tabs_list.*
 
 
-class TabsListFragment : BaseFragment<TabsListViewModel>() {
+class TabsListFragment : BaseFragment<TabsListViewModel>(), View.OnClickListener {
     private lateinit var adapter: TabsListAdapter
     private lateinit var navigator: Navigator
     override val viewModelClass = TabsListViewModel::class
@@ -38,10 +47,62 @@ class TabsListFragment : BaseFragment<TabsListViewModel>() {
 
         fragment_tabs_list_viewpager.adapter = adapter
         fragment_tabs_list_tabs.setupWithViewPager(fragment_tabs_list_viewpager)
+
+        fragment_event_list_fab_menu.setOnClickListener(this)
+        fragment_event_list_fab_qr_code.setOnClickListener(this)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_tabs_list, container, false)
+    }
+
+    override fun onClick(v: View) {
+
+        when (v.id) {
+            R.id.fragment_event_list_fab_qr_code -> {
+                val options = QuickPermissionsOptions()
+                options.handleRationale = true
+                options.rationaleMessage = "Nous avons vraiment besoin de ta caméra"
+                options.permanentDeniedMethod = { permissionsPermanentlyDenied(it) }
+                startQRScanner(options)
+            }
+            R.id.fragment_event_list_fab_create_event -> {
+
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(activity, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Log.d("QRCODE", result.contents)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun permissionsPermanentlyDenied(req: QuickPermissionsRequest) {
+        // this will be called when some/all permissions required by the method are permanently
+        // denied. Handle it your way.
+        AlertDialog.Builder(context!!)
+            .setTitle("Permission refusée")
+            .setMessage("This is the custom permissions permanently denied dialog. " +
+                    "Please open app settings to open app settings for allowing permissions, " +
+                    "or cancel to end the permission flow.")
+            .setPositiveButton("App Settings") { dialog, which -> req.openAppSettings() }
+            .setNegativeButton("Cancel") { dialog, which -> req.cancel() }
+            .setCancelable(false)
+            .show()
+    }
+
+    fun startQRScanner(options: QuickPermissionsOptions) = runWithPermissions(Manifest.permission.CAMERA, options = options) {
+        IntentIntegrator(activity).initiateScan()
     }
 
     companion object {
